e5ec0640b0510fb4fab2d4197ba5a7bc79d843f3
gklp <gokalpkuscu@gmail.com>
2018-12-12 22:19:09 +0300

    1

--- helmj-core/src/main/java/com/helmj/support/client/model/request/RequestVersion.java
+++
@@ -1,7 +0,0 @@
-package com.helmj.support.client.model.request;
-
-import lombok.Builder;
-
-@Builder
-public class RequestVersion {
-}
--- helmj-core/src/main/java/com/helmj/support/client/operation/Operation.java
+++
@@ -1,8 +0,0 @@
-package com.helmj.support.client.operation;
-
-@FunctionalInterface
-public interface Operation<Req, Res> {
-
-	Res execute(Req req);
-
-}
